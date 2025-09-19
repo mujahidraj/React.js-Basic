a6c90d2847a05233100d775700d1b14dde96f70a
mujahidraj <mujahidraj65@gmail.com>
2025-09-19 17:04:48 +0600

    first react app

---
+++ Module - 38 (React core concept part 1)/explore-core-react/test.jsx
@@ -0,0 +1,39 @@
+// React is a library of the javascript for building a user interface 
+// react is declarative , efficient , and flexible 
+/*
+
+it is fast and component base
+
+develop and maintained by facebook 
+
+react use the virtual dom
+
+react can be used in ecommerce , qa website , business website , social media , dashboard 
+
+react use JSX template for ui 
+
+focus on the view in MVC 
+
+one way data binding
+
+use vertual dom 
+
+for routing use react router
+
+for state masnagement use redux , recoil or context api
+
+
+
+*/
+
+//  to open a project forlder  :  npm create vite@latest my-react-app -- --template react
+
+/*
+
+1 . component is a building block 
+2 . every piece of ui is a component 
+3 . component is a java script function
+4 . their name always begin with a capital letter 
+5 . they return jsx markup 
+
+*/
