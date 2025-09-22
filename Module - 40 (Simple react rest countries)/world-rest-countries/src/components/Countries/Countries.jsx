b1231cdea2557d0856a0a7cfa9da1af44ccc074a
mujahidraj <mujahidraj65@gmail.com>
2025-09-22 20:33:01 +0600

    Use style in react and 3 column layout

--- Module - 40 (Simple react rest countries)/world-rest-countries/src/components/Countries/Countries.jsx
+++ Module - 40 (Simple react rest countries)/world-rest-countries/src/components/Countries/Countries.jsx
@@ -1,14 +1,19 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './countries.css'
 
-const Countries = ({countriesFetch}) => {
-    const countries = use (countriesFetch);
+const Countries = ({ countriesFetch }) => {
+    const countries = use(countriesFetch);
 
-    
+
+
     return (
         <div>
             <h2>Countries : {countries.length}</h2>
-            {countries.map(country => <Country key={country.cca3} country={country}></Country>)}
+            <div className='countries'>
+                {countries.map(country => <Country key={country.cca3} country={country}></Country>)}
+            </div>
+
         </div>
     );
 };
--- Module - 40 (Simple react rest countries)/world-rest-countries/src/components/Country/Country.jsx
+++ Module - 40 (Simple react rest countries)/world-rest-countries/src/components/Country/Country.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import './country.css'
 
 const Country = ({country}) => {
     
+    const handleVisited =()=>{
+        alert("Are sure your are visited?")
+        
+    }
     
     return (
-        <div>
+        <div className='country'>
             <h3>Name : {country.name.common} </h3>
             <img src={country.flags.png} alt="" />
             <h5>LandLocked : {country.landlocked  ? 'YES' : 'NO'}</h5>
             <h5>Population : {country.population}</h5>
+            <button onClick={handleVisited}>Not visited</button>
         </div>
     );
 };
