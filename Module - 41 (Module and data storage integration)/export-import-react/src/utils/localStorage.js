182c64f5607acd4995f2ee07e42fbdfa178a4a06
mujahidraj <mujahidraj65@gmail.com>
2025-09-24 22:36:51 +0600

    introduction to local storage and load stored id

---
+++ Module - 41 (Module and data storage integration)/export-import-react/src/utils/localStorage.js
@@ -0,0 +1,25 @@
+
+
+
+const getCartFromLocalStorage =()=>{
+    const storedCart = localStorage.getItem('cart')
+    if(storedCart){
+        const parsedStoredCart = JSON.parse(storedCart);
+        return parsedStoredCart;
+    }
+    return [];
+}
+
+const saveToLocalStorage=cart =>{
+    const jsonCart = JSON.stringify(cart)
+    localStorage.setItem('cart',jsonCart)
+
+}
+
+const addItemToCartLocalStorage =id=>{
+    const cart = getCartFromLocalStorage();
+    const newCart = [...cart , id]
+    saveToLocalStorage(newCart)
+
+}
+export {getCartFromLocalStorage , addItemToCartLocalStorage}
