5d550be3acbbe348d77472df6fede0d64d7050ea
mujahidraj <mujahidraj65@gmail.com>
2025-11-03 22:27:11 +0600

    nav bar

--- Module - 44 (Tailwind css , axious , rechart and awesome component)/tailwind-react-pricing-option/src/App.jsx
+++ Module - 44 (Tailwind css , axious , rechart and awesome component)/tailwind-react-pricing-option/src/App.jsx
@@ -1,17 +1,16 @@
 
+
 import './App.css'
+import NavBar from './Components/nav-bar/NavBar'
 
 function App() {
 
   return (
     <>
-      <h2 className='poppins text-3xl'>Hello world</h2>
-      <ul class="steps">
-        <li class="step step-primary">Register</li>
-        <li class="step step-primary">Choose plan</li>
-        <li class="step">Purchase</li>
-        <li class="step">Receive Product</li>
-      </ul>
+    <div>
+      <NavBar></NavBar>
+      
+    </div>
     </>
   )
 }
---
+++ Module - 44 (Tailwind css , axious , rechart and awesome component)/tailwind-react-pricing-option/src/Components/nav-bar/NavBar.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { CiMenuBurger } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
+
+
+
+const navItems = [
+  {
+    id: 1,
+    name: 'Home',
+    path: '/'
+  },
+  {
+    id: 2,
+    name: 'About',
+    path: '/about'
+  },
+  {
+    id: 3,
+    name: 'Services',
+    path: '/services'
+  },
+  {
+    id: 4,
+    name: 'Portfolio',
+    path: '/portfolio'
+  },
+  {
+    id: 5,
+    name: 'Blog',
+    path: '/blog'
+  },
+  {
+    id: 6,
+    name: 'Contact',
+    path: '/contact'
+  }
+];
+
+const NavBar = () => {
+
+  const [menu, setMenu] = useState(false)
+
+
+  return (
+    <div>
+      <nav className='flex justify-between mx-10 items-center'>
+
+        <span className='flex flex-row gap-5 justify-between items-center' onClick={() => setMenu(!menu)}>
+          {
+            menu ? <IoClose className='md:hidden' ></IoClose>
+              : <CiMenuBurger className='md:hidden' />
+          }
+          <h3>My NavBar</h3>
+        </span>
+
+
+
+        <ul className='md:flex hidden'>
+          {
+            navItems.map(items => <li className='ml-6'><a href={items.path}>{items.name}</a></li>)
+          }
+        </ul>
+        <button>
+          login
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
