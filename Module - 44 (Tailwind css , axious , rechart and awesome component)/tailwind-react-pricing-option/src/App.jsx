9dac088faf225298388755701ed54200d4160c96
mujahidraj <mujahidraj65@gmail.com>
2025-11-05 22:34:17 +0600

    chart

--- Module - 44 (Tailwind css , axious , rechart and awesome component)/tailwind-react-pricing-option/src/App.jsx
+++ Module - 44 (Tailwind css , axious , rechart and awesome component)/tailwind-react-pricing-option/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import NavBar from './Components/nav-bar/NavBar'
 import PricingOption from './Components/PricingOption/PricingOption'
 import Pricing_display from './Components/PricingOption/Pricing_display'
+import ResultGraph from './Components/Graph/ResultGraph'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Suspense fallback={<span class="loading loading-ring loading-lg"></span>}>
         <PricingOption></PricingOption></Suspense>
         
+        <ResultGraph></ResultGraph>
       </div>
     </>
   )
---
+++ Module - 44 (Tailwind css , axious , rechart and awesome component)/tailwind-react-pricing-option/test.jsx
@@ -0,0 +1,7 @@
+// 1 . react 
+// 2 . tailwind
+// 3 . daisy
+// 4 . lucide
+// 5 . rechart
+// 6 . react icons
+// 5 . axios
