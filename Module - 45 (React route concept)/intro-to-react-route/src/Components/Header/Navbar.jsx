49603a803622cf1090a9c66060453e769f062f83
mujahidraj <mujahidraj65@gmail.com>
2025-11-06 20:42:59 +0600

    nav link , link , anchor usage

--- Module - 45 (React route concept)/intro-to-react-route/src/Components/Header/Navbar.jsx
+++ Module - 45 (React route concept)/intro-to-react-route/src/Components/Header/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Link, NavLink, Outlet } from 'react-router';
+import MainContent from '../MainContent/MainContent';
+import MainContent2 from '../MainContent2/MainContent2';
 
 const Navbar = () => {
   return (
     <div>
       <h2>My name is navbar</h2>
+      {/* <a href="/">Home</a><a href="/MainContent">Main Content</a><a href="MainContent2">Main Content 2</a> */}
+     
+      {/* <Link to="MainContent">Main Contain</Link>
+      <Link to="MainContent2">Main Contain 2</Link>
+      <Link to="/">Home Contain</Link> */}
+
+      <NavLink to="/">Home Content</NavLink>
+      <NavLink to="MainContent">Main Content</NavLink>
+      <NavLink to="MainContent2">Main Content 2</NavLink>
+
       <Outlet></Outlet>
     </div>
   );
