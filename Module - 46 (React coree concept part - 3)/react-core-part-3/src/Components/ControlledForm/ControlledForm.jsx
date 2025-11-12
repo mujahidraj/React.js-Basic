ef701f9242247db344c77f48787cc07e9cc3c0f7
mujahidraj <mujahidraj65@gmail.com>
2025-11-12 12:29:18 +0600

    controlled form

--- Module - 46 (React coree concept part - 3)/react-core-part-3/src/Components/ControlledForm/ControlledForm.jsx
+++ Module - 46 (React coree concept part - 3)/react-core-part-3/src/Components/ControlledForm/ControlledForm.jsx
@@ -1,28 +1,45 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+
 
 const ControlledForm = () => {
 
   const [password , setPassword] = useState("")
+  const [error , setError] = useState("")
 
-  const handleControlledForm = (e)=>{
+
+  const handleSubmit =(e)=>{
     e.preventDefault()
-    console.log("hello world");
+    if(!password.toLocaleUpperCase){
+      setError("Fuck man ! why no upper case.")
+    }
+    else {
+      setError("perfect")
+    }
     
   }
 
-  const handlePasswordChange = (e) =>{
+  const handleFormOnChange =(e)=>{
     console.log(e.target.value);
+    setPassword(e.target.value)
+    if(password.length <=8){
+      setError("Who the hell are you? write the 8 length password.")
+    }
+    else{
+      setError("good job! go ahead.")
+    }
     
   }
 
   return (
     <div>
-      <form onSubmit={handleControlledForm}>
-        <input type="email" name="email" placeholder='Your Email' required /><br />
-        <input type="password" name="password"  onChange={handlePasswordChange} defaultValue={password} placeholder='Your Password' required />
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Your Email" required />
+        <br />
+        <input type="password" name="password" onChange={handleFormOnChange} defaultValue={password} placeholder="Your Password" required />
         <br />
         <input type="submit" value="Submit" />
       </form>
+      <p><small>{error}</small></p>
     </div>
   );
 };
