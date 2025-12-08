e41f80a039f31607683b04f98b3e9165a5aef6b0
mujahidraj <mujahidraj65@gmail.com>
2025-12-08 21:06:09 +0600

    password validation

--- Module - 50 (Email password authentication and login form)/email-password-auth/src/Components/Register/Register.jsx
+++ Module - 50 (Email password authentication and login form)/email-password-auth/src/Components/Register/Register.jsx
@@ -1,48 +1,62 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
 
-const handleSubmit = e=>{
-  e.preventDefault()
-const email = e.target.email.value;
-const password = e.target.password.value;
-console.log(email , "and" , password);
 
-createUserWithEmailAndPassword (auth, email, password)
-.then(result =>{
-  console.log(result);
-  
-})
-.catch(error=>{
-  console.log(error);
-  
-})
-
-}
 
 const Register = () => {
+
+  const [errorMsg, setErrorMsg] = useState(' ');
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, "and", password);
+
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,32}$/;
+
+    if (passwordRegex.test(password) === false) {
+      setErrorMsg('Password is not valid. It must be 8-32 characters long and include at least one uppercase letter, one number, and one special character.');
+      return;
+    }
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        console.log(result);
+        setErrorMsg(' ')
+
+      })
+      .catch(error => {
+        console.log(error);
+        setErrorMsg(error.code)
+
+      })
+
+  }
   return (
     <div className='border p-5 max-w-sm mx-auto'>
       <form onSubmit={handleSubmit}>
         {/* Email */}
-        
-          <label className="input validator join-item">
-            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-              </g>
-            </svg>
-            <input type="email" name='email' placeholder="mail@site.com" required />
-          </label>
-          <div className="validator-hint hidden">Enter valid email address</div>
-       
+
+        <label className="input validator join-item">
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input type="email" name='email' placeholder="mail@site.com" required />
+        </label>
+        <div className="validator-hint hidden">Enter valid email address</div>
+
 
         <br />
         {/* Password */}
@@ -78,6 +92,9 @@ const Register = () => {
         <br />
         <button className="btn btn-primary" type="submit">Register</button>
       </form>
+      {
+        errorMsg && <p className='text-red-700'>{errorMsg.split("/")}</p>
+      }
     </div>
   );
 };
