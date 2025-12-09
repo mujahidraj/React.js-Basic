4ce4d81fc9ac736351fc2dd71b4206ca6d62ed66
mujahidraj <mujahidraj65@gmail.com>
2025-12-09 19:19:57 +0600

    project setup done

---
+++ Module - 51 (React auth integration and private route)/auth-integration/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      i am home
+    </div>
+  );
+};
+
+export default Home;
--- Module - 51 (React auth integration and private route)/auth-integration/src/Routes/routes.jsx
+++ Module - 51 (React auth integration and private route)/auth-integration/src/Routes/routes.jsx
@@ -1,9 +1,28 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from "../Layout/Root";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component : Root,
+    children : [
+      {
+        path : "/",
+        index : true,
+        Component : Home 
+      },
+      {
+        path : "login",
+        Component : Login
+      },
+      {
+        path : "register",
+        Component : Register
+      }
+    ]
   },
 ]);
